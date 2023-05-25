ba50d4717ecc2ab6e772337d1a42d44bfac26954
jinglebelline <93650030+jinglebelline@users.noreply.github.com>
2023-05-25 09:53:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+//const ejs = require("ejs")
+
+const app = express()
+
+const port = process.env.PORT || 2000;
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(port,()=>{
+    console.log(`Example app listening at ${port}`)
+})
