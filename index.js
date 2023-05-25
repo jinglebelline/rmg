61edb72e0ed6c3cb6e4b21ca952f5e366f74f3c2
jinglebelline <93650030+jinglebelline@users.noreply.github.com>
2023-05-25 10:20:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 
 const port = process.env.PORT || 2000;
 
-
+app.use(express.static('build'))
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
